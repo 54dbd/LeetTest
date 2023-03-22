674ce413512ebf69c95b231a3566c5c92d34ad60
54dbd <845559360@qq.com>
2023-03-22 17:38:26 +0800

    fix:change footer

--- LeetTest_client/src/views/LandingPages/SignUp/SingUpView.vue
+++ LeetTest_client/src/views/LandingPages/SignUp/SingUpView.vue
@@ -111,7 +111,7 @@ onMounted(() => {
                       variant="gradient"
                       color="success"
                       fullWidth
-                      >完成登录</MaterialButton
+                      >完成注册</MaterialButton
                     >
                   </div>
                 </form>
@@ -127,47 +127,9 @@ onMounted(() => {
               <div
                 class="copyright text-center text-sm text-white text-lg-start"
               >
-                © {{ new Date().getFullYear() }}, made JavaEE Project with
+                © {{ new Date().getFullYear() }}, made for a JavaEE project with
                 <i class="fa fa-heart" aria-hidden="true"></i>
               </div>
-            </div>
-            <div class="col-12 col-md-6">
-              <ul
-                class="nav nav-footer justify-content-center justify-content-lg-end"
-              >
-                <li class="nav-item">
-                  <a
-                    href="https://www.creative-tim.com"
-                    class="nav-link text-white"
-                    target="_blank"
-                    >Creative Tim</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="https://www.creative-tim.com/presentation"
-                    class="nav-link text-white"
-                    target="_blank"
-                    >About Us</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="https://www.creative-tim.com/blog"
-                    class="nav-link text-white"
-                    target="_blank"
-                    >Blog</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="https://www.creative-tim.com/license"
-                    class="nav-link pe-0 text-white"
-                    target="_blank"
-                    >License</a
-                  >
-                </li>
-              </ul>
             </div>
           </div>
         </div>
