5a58db78ec754250c61af2bbd1826ec6a84131d4
Bastian-crux <std1006966813@163.com>
2023-05-22 22:42:43 +0800

    feat: timeline style

--- LeetTest_client/src/components/myComponents/TeacherList.vue
+++ LeetTest_client/src/components/myComponents/TeacherList.vue
@@ -1,5 +1,5 @@
 <script setup xmlns:slot="http://www.w3.org/1999/XSL/Transform">
-import {reqGetTeacherList, reqSearchTeacher} from "@/api";
+import { reqGetTeacherList, reqSearchTeacher } from "@/api";
 </script>
 
 <template>
@@ -10,57 +10,47 @@ import {reqGetTeacherList, reqSearchTeacher} from "@/api";
       </el-col>
       <el-col :span="24" :offset="2">
         <el-input
-            type="text"
-            placeholder="请输入内容"
-            v-model="search"
-            style="width: 200px;"
-            size="small"
+          type="text"
+          placeholder="请输入内容"
+          v-model="search"
+          style="width: 200px"
+          size="small"
         />
         <el-table
-            @cell-click="toTeacherDetail"
-            highlight-current-row
-            :data="teacherList"
-            style="width: 100%;">
-          <el-table-column
-              prop="id"
-              label="序号"
-              type="index"
-              width="100">
+          @cell-click="toTeacherDetail"
+          highlight-current-row
+          :data="teacherList"
+          style="width: 100%"
+        >
+          <el-table-column prop="id" label="序号" type="index" width="100">
           </el-table-column>
-          <el-table-column
-              prop="tname"
-              label="教师姓名"
-              width="200">
+          <el-table-column prop="tname" label="教师姓名" width="200">
             <template v-slot:default="scope">
               <slot>
                 <el-tag size="medium">{{ scope.row.tname }}</el-tag>
               </slot>
             </template>
           </el-table-column>
-          <el-table-column
-              prop="studydir"
-              label="专业方向"
-              width="500">
+          <el-table-column prop="studydir" label="专业方向" width="500">
             <template v-slot:default="scope">
               <slot v-if="scope.row.studydir">
                 {{ scope.row.studydir }}
               </slot>
-              <slot v-if="!scope.row.studydir">
-                暂无
-              </slot>
+              <slot v-if="!scope.row.studydir"> 暂无 </slot>
             </template>
           </el-table-column>
         </el-table>
       </el-col>
       <el-col :span="20" style="display: flex; align-items: center; justify-content: center">
         <el-pagination
-            style="text-align: center; margin-top: 10px;"
-            :page-size="5"
-            :current-page="currentPage"
-            small
-            @current-change="currentChange"
-            layout="prev, pager, next"
-            :total="total">
+          style="text-align: center; margin-top: 10px"
+          :page-size="5"
+          :current-page="currentPage"
+          small
+          @current-change="currentChange"
+          layout="prev, pager, next"
+          :total="total"
+        >
         </el-pagination>
       </el-col>
     </el-row>
@@ -70,19 +60,19 @@ import {reqGetTeacherList, reqSearchTeacher} from "@/api";
 <script>
 export default {
   name: "TeacherList",
-  props: ['schoolIntroduce'],
+  props: ["schoolIntroduce"],
   data() {
     return {
       teacherList: [],
       currentPage: 1,
       pages: 0,
       total: 0,
-      search: '',
-      sname: ''
-    }
+      search: "",
+      sname: "",
+    };
   },
   mounted() {
-    this.getTeacherList()
+    this.getTeacherList();
   },
   methods: {
     toTeacherDetail(item) {
@@ -90,47 +80,50 @@ export default {
         path: "/teacherDetail",
         query: {
           tid: item.id,
-          sname: this.sname
-        }
-      })
+          sname: this.sname,
+        },
+      });
     },
     async getTeacherList() {
-      const result = await reqGetTeacherList(this.$route.query.sid, this.currentPage)
+      const result = await reqGetTeacherList(
+        this.$route.query.sid,
+        this.currentPage
+      );
       if (result.data.code === 200) {
-        this.teacherList = result.data.data.teacherVoList
-        this.total = result.data.data.total
-        this.pages = result.data.data.pages
+        this.teacherList = result.data.data.teacherVoList;
+        this.total = result.data.data.total;
+        this.pages = result.data.data.pages;
       } else {
-        this.$message.warning('系统异常~ ' + result.data.msg)
+        this.$message.warning("系统异常~ " + result.data.msg);
       }
     },
     currentChange(currentPage) {
-      this.currentPage = currentPage
+      this.currentPage = currentPage;
       if (this.search) {
-        this.searchTeacher()
+        this.searchTeacher();
       } else {
-        this.getTeacherList()
+        this.getTeacherList();
       }
     },
     async searchTeacher(current) {
       if (current) {
-        this.currentPage = current
+        this.currentPage = current;
       }
       const result = await reqSearchTeacher({
         text: this.search,
         current: this.currentPage,
         size: 5,
         sid: this.$route.query.sid,
-        uid: this.$store.state.user.userInfo.userId
-      })
+        uid: this.$store.state.user.userInfo.userId,
+      });
       if (result.data.code === 200) {
-        this.teacherList = result.data.data.teacherVoList
-        this.total = result.data.data.total
-        this.pages = result.data.data.pages
+        this.teacherList = result.data.data.teacherVoList;
+        this.total = result.data.data.total;
+        this.pages = result.data.data.pages;
       } else {
-        this.$message.warning('系统异常~ ' + result.data.msg)
+        this.$message.warning("系统异常~ " + result.data.msg);
       }
-    }
+    },
   },
   watch: {
     search() {
@@ -138,9 +131,9 @@ export default {
     },
     schoolIntroduce(sc) {
       this.sname = sc.sname;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
